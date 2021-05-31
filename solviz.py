8758ea011053a838595f10e68aca446beef1cafd
mindyourbots <67726f@gmail.com>
2021-05-30 22:52:45 -0700

    getting more stuff done

--- solviz.py
+++ solviz.py
@@ -1,13 +1,22 @@
-import datetime
+import datetime as dt
 import pandas as pd
 import os
 print("Starting at: ",os.getcwd())
 d = {}
 def menu():
-    print("I'm the main menue")
-    print("What would you like to do?")
     print('''
-            Welcome to SOLVIZ, visualize your solar production''')
+            Welcome to SOLVIZ, visualize your solar production
+           --------------------------------------------------- 
+           SAVED USER DATA
+            ''')
+    today = dt.date.today()
+    sol_install = dt.date(2008, 7, 14)
+    days_since_install = (today - sol_install).days
+    print("Today is:", today, "and it has been", days_since_install, "days since your installation")
+    print("Last batch process contained",3,"days of sun above 50 %")
+    print("""
+    ------------------------------------------------------------------------""")
+
 
 def import_data():
     """Take and combine excel files from the INPUT folder and place a
@@ -29,19 +38,39 @@ def import_data():
     df.to_excel(writer)
     writer.save()
 
-def Organize_Output_File():
-    print(Organizing the files)
+def file_play():
+    print("Organizing the files, finding DIR")
+    print(os.getcwd())
+    os.chdir('OUTPUT')
+    print(os.getcwd())
+    print("Getting files...........this can take a bit..")
+    elect = pd.read_excel(r'output.xlsx')
+    print("done getting...")
+    return elect
 
+
+def Organize_Output_File():
+    print("Organizing the files, finding DIR")
+    print(os.getcwd())
+    os.chdir('OUTPUT')
+    print(os.getcwd())
+    print("Getting files...........this can take a bit..")
+    elect = pd.read_excel(r'output.xlsx')
+    print(elect.head())
+    print("Total size : ", elect.shape)
+
+
+def fin():
+    print("PROGRAM END....")
 def main():
     menu()
     #Bring in all of the excel files and place collated vers. in OUTPUT
     #import_data()
-
     #Organize the Output file so that all the dates are in the right order
-    Organize_Output_File()
-
+    #Organize_Output_File()
     #Display the data and save some graphs to the OUTPUT folder with date-stamp.
-
+    file_play()
+    fin()
 
 if __name__ == "__main__":
     main()
